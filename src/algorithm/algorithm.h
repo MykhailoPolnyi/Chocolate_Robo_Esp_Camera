e0ac6624d58ea126626a146233e764e9fcaf6aac
MykhailoPolnyi <mykhailo.polnyi.knm.2020@lpnu.ua>
2023-11-05 14:03:35 +0200

    Add logic for route saving

---
+++ src/algorithm/algorithm.h
@@ -0,0 +1,8 @@
+#if !defined(CAMERA_ALGORITHM_MAIN)
+#define CAMERA_ALGORITHM_MAIN
+
+void start_algorithm_writing();
+
+int add_new_point(int x, int y);
+
+#endif // CAMERA_ALGORITHM_MAIN
--- src/command/command.h
+++ src/command/command.h
@@ -6,8 +6,13 @@
 #define SERVO_CMD_DOWN "down"
 #define SERVO_CMD_LEFT "left"
 #define SERVO_CMD_RIGHT "right"
+#define SERVO_CMD_FOLLOW_ROUTE "route"
 
 int cmd_move(char* new_direction);
+
+int cmd_refresh_record();
+
+int cmd_add_point();
 
 char* get_current_direction();
 
